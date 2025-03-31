3b66a7eaedac1d3e5106b632bd1614ba724504f6
Navjot Kaur <navjotkaur1440@gmail.com>
2025-03-31 23:33:49 +0530

    third commit

---
+++ tailwind.config.js
@@ -0,0 +1,17 @@
+module.exports = {
+    theme: {
+      extend: {
+        screens: {
+          'navjot': '3690px',
+        },
+        spacing: {
+          'c-nav': '11px',
+          'navjott': '10px',
+          '100': '100px',
+          'custom-n': '1000px',
+        },
+      },
+    },
+    plugins: [],
+  };
+  
